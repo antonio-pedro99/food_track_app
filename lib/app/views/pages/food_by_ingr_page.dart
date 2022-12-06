aa2bc964f9f2f3809e50fdc84147138d33603679
antonio-pedro99 <tonio.pedro99@gmail.com>
2022-12-06 15:04:08 +0530

    by ingredients page done

--- lib/app/views/pages/food_by_ingr_page.dart
+++ lib/app/views/pages/food_by_ingr_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_track_app/app/views/widgets/text_field.dart';
 
 class FoodByIngridPage extends StatefulWidget {
   const FoodByIngridPage({super.key, required this.title});
@@ -10,6 +11,10 @@ class FoodByIngridPage extends StatefulWidget {
 }
 
 class _FoodByIngridPageState extends State<FoodByIngridPage> {
+  final TextEditingController ingredientsEditingController =
+      TextEditingController();
+  final TextEditingController nutritionEditingController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,17 +24,31 @@ class _FoodByIngridPageState extends State<FoodByIngridPage> {
           IconButton(onPressed: () {}, icon: const Icon(Icons.receipt_long))
         ],
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            CustomTextField(
+              controller: ingredientsEditingController,
+              hint: "Enter any ingredients",
+              icon: Icons.search,
             ),
-            Text(
-              "Hello World",
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(height: 8),
+            CustomTextField(
+              controller: ingredientsEditingController,
+              hint: "Enter any nutrition",
+              icon: Icons.search,
             ),
+            const SizedBox(height: 24),
+            Flexible(
+                child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Container(
+                          margin: const EdgeInsets.only(bottom: 8),
+                          height: 100,
+                          color: Colors.primaries[index % 10]);
+                    }))
           ],
         ),
       ),
